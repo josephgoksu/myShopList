1b8cb6437fb49a67de371fd00b3bb1eedf79b12b
y7usuf <me@yusufgoksu.com>
2018-05-21 21:59:25 +0300

    delete unnecessary state

--- app/src/ActiveList.js
+++ app/src/ActiveList.js
@@ -3,10 +3,6 @@ import React, { Component } from 'react';
 class ActiveItems extends Component {
 	constructor(props) {
 		super(props);
-
-		// this.state = {
-		// 	items: []
-		// };
 	}
 
 	delete(key) {
